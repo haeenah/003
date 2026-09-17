4584bcf558a98e7e0739a1dba890a4a5b3332b84
haeenah <haeenah629@sookmyung.ac.kr>
2026-09-17 16:36:26 +0900

    실습 3 최종본

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
     char c;
+    //int c1;
 
     printf("input a character :");
     scanf("%c", &c);
 
-    c = c + 1;
-
-    printf("The next character is %c (%i)\n", c, c);
+    //c1 = c + 1;
+    printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
 
     return 0;
 }
